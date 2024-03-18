2a5540b71c2ad49a19a01279e6baa36dd3486cb7
Keane Tan <chin.lung.t93@gmail.com>
2024-03-18 10:36:33 -0500

    Added config and cross section files for TreeMaker production of SVJ tchannel signals

---
+++ Production/test/condorSub/dict_Summer20UL16APV_svj_tch.py
@@ -0,0 +1,18 @@
+flist = {
+    "scenario": "Summer20UL16APVsig",
+    "samples": [
+        ['PrivateSamples.SVJ_UL2016APV_t-channel_mMed-600_mDark-20_rinv-0p3_alpha-peak_yukawa-1_13TeV-madgraphMLM-pythia8_n-1000'],
+        ['PrivateSamples.SVJ_UL2016APV_t-channel_mMed-800_mDark-20_rinv-0p3_alpha-peak_yukawa-1_13TeV-madgraphMLM-pythia8_n-1000'],
+        ['PrivateSamples.SVJ_UL2016APV_t-channel_mMed-1000_mDark-20_rinv-0p3_alpha-peak_yukawa-1_13TeV-madgraphMLM-pythia8_n-1000'],
+        ['PrivateSamples.SVJ_UL2016APV_t-channel_mMed-1500_mDark-20_rinv-0p3_alpha-peak_yukawa-1_13TeV-madgraphMLM-pythia8_n-1000'],
+        ['PrivateSamples.SVJ_UL2016APV_t-channel_mMed-2000_mDark-1_rinv-0p3_alpha-peak_yukawa-1_13TeV-madgraphMLM-pythia8_n-1000'],
+        ['PrivateSamples.SVJ_UL2016APV_t-channel_mMed-2000_mDark-20_rinv-0p1_alpha-peak_yukawa-1_13TeV-madgraphMLM-pythia8_n-1000'],
+        ['PrivateSamples.SVJ_UL2016APV_t-channel_mMed-2000_mDark-20_rinv-0p3_alpha-peak_yukawa-1_13TeV-madgraphMLM-pythia8_n-1000'],
+        ['PrivateSamples.SVJ_UL2016APV_t-channel_mMed-2000_mDark-20_rinv-0p5_alpha-peak_yukawa-1_13TeV-madgraphMLM-pythia8_n-1000'],
+        ['PrivateSamples.SVJ_UL2016APV_t-channel_mMed-2000_mDark-20_rinv-0p7_alpha-peak_yukawa-1_13TeV-madgraphMLM-pythia8_n-1000'],
+        ['PrivateSamples.SVJ_UL2016APV_t-channel_mMed-2000_mDark-50_rinv-0p3_alpha-peak_yukawa-1_13TeV-madgraphMLM-pythia8_n-1000'],
+        ['PrivateSamples.SVJ_UL2016APV_t-channel_mMed-2000_mDark-100_rinv-0p3_alpha-peak_yukawa-1_13TeV-madgraphMLM-pythia8_n-1000'],
+        ['PrivateSamples.SVJ_UL2016APV_t-channel_mMed-3000_mDark-20_rinv-0p3_alpha-peak_yukawa-1_13TeV-madgraphMLM-pythia8_n-1000'],
+        ['PrivateSamples.SVJ_UL2016APV_t-channel_mMed-4000_mDark-20_rinv-0p3_alpha-peak_yukawa-1_13TeV-madgraphMLM-pythia8_n-1000'],
+    ]
+}
